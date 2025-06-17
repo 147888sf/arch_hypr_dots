e6bdd3f0489e296a5b512ccbbec59b6a3c77c7a8
Vova_4104 <4104vova@gmail.com>
2025-06-17 19:36:50 +0300

    Code cleanup

--- modules/post_install.py
+++ modules/post_install.py
@@ -8,88 +8,106 @@ def cmdrun(command, cwd):
     except subprocess.CalledProcessError:
         pass
 
+
 def post_install(do_reboot, do_ly_dm):
-	home = os.getenv('HOME')
+    home = os.getenv("HOME")
 
-	waybar_css = f'{home}/.config/waybar/style.css'
-	wallpapers_conf = f'{home}/.config/hypr/wallpapers.conf'
-	hyprpaper_conf = f'{home}/.config/hypr/hyprpaper.conf'
-	multilib_conf = '/etc/pacman.conf'
+    waybar_css = f"{home}/.config/waybar/style.css"
+    wallpapers_conf = f"{home}/.config/hypr/wallpapers.conf"
+    hyprpaper_conf = f"{home}/.config/hypr/hyprpaper.conf"
+    multilib_conf = "/etc/pacman.conf"
 
-	# Waybar config
-	if not os.access(waybar_css, os.R_OK | os.W_OK):
-		cmdrun(f'sudo chown $USER:$USER {waybar_css} && chmod 644 {waybar_css}', os.path.expanduser('~'))
+    # Waybar config
+    if not os.access(waybar_css, os.R_OK | os.W_OK):
+        cmdrun(
+            f"sudo chown $USER:$USER {waybar_css} && chmod 644 {waybar_css}",
+            os.path.expanduser("~"),
+        )
 
-	file = open(waybar_css, 'rb')
-	content = file.read().decode()
-	file.close()
-	file = open(waybar_css, 'wb')
-	file.write(content.replace('$HOME',home).encode())
-	file.close()
+    file = open(waybar_css, "rb")
+    content = file.read().decode()
+    file.close()
+    file = open(waybar_css, "wb")
+    file.write(content.replace("$HOME", home).encode())
+    file.close()
 
+    # Walpaper set
+    image = f"{home}/Wallpapers/urban.png"
 
+    cmdrun(f"wal -i {image}", os.path.expanduser("~"))
 
-	# Walpaper set
-	image = f'{home}/Wallpapers/urban.png'
+    if not os.access(wallpapers_conf, os.R_OK | os.W_OK):
+        cmdrun(
+            f"sudo chown $USER:$USER {wallpapers_conf} && chmod 644 {wallpapers_conf}",
+            os.path.expanduser("~"),
+        )
 
-	cmdrun(f'wal -i {image}', os.path.expanduser('~'))
+    file = open(wallpapers_conf, "wb")
+    file.write(f"$wallpaper = {image}".encode())
+    file.close()
 
-	if not os.access(wallpapers_conf, os.R_OK | os.W_OK):
-		cmdrun(f'sudo chown $USER:$USER {wallpapers_conf} && chmod 644 {wallpapers_conf}', os.path.expanduser('~'))
+    if not os.access(hyprpaper_conf, os.R_OK | os.W_OK):
+        cmdrun(
+            f"sudo chown $USER:$USER {hyprpaper_conf} && chmod 644 {hyprpaper_conf}",
+            os.path.expanduser("~"),
+        )
 
-	file = open(wallpapers_conf,'wb')
-	file.write(f'$wallpaper = {image}'.encode())
-	file.close()
+    file = open(hyprpaper_conf, "wb")
+    file.write(f"preload = {image}\nwallpaper = , {image}".encode())
+    file.close()
 
-	if not os.access(hyprpaper_conf, os.R_OK | os.W_OK):
-		cmdrun(f'sudo chown $USER:$USER {hyprpaper_conf} && chmod 644 {hyprpaper_conf}', os.path.expanduser('~'))
+    # Multilib enable
+    try:
+        if not os.access(multilib_conf, os.R_OK | os.W_OK):
+            os.system(
+                f"sudo chown $USER:$USER {multilib_conf} && chmod 644 {multilib_conf}"
+            )
 
-	file = open(hyprpaper_conf,'wb')
-	file.write(f'preload = {image}\nwallpaper = , {image}'.encode())
-	file.close()
+        file = open(multilib_conf, "rb")
+        content = file.read().decode()
+        file.close()
+        file = open(multilib_conf, "wb")
+        file.write(
+            content.replace(
+                "#[multilib]\n#Include = /etc/pacman.d/mirrorlist",
+                "[multilib]\nInclude = /etc/pacman.d/mirrorlist",
+            ).encode()
+        )
+        file.close()
+    except Exception:
+        pass
 
+    # Screenshare
+    cmdrun(
+        "sudo systemctl --user enable --now pipewire pipewire-pulse wireplumber",
+        os.path.expanduser("~"),
+    )
 
+    # Network manager
+    cmdrun("sudo systemctl enable NetworkManager.service", os.path.expanduser("~"))
 
-	# Multilib enable
-	try:
-		if not os.access(multilib_conf, os.R_OK | os.W_OK):
-			os.system(f'sudo chown $USER:$USER {multilib_conf} && chmod 644 {multilib_conf}')
+    # Default dark mode
+    cmdrun(
+        "gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark",
+        os.path.expanduser("~"),
+    )
+    cmdrun(
+        "gsettings set org.gnome.desktop.interface color-scheme prefer-dark",
+        os.path.expanduser("~"),
+    )
+    cmdrun(
+        "gsettings set org.gnome.desktop.interface icon-theme Papirus",
+        os.path.expanduser("~"),
+    )
+    cmdrun(
+        "gsettings set org.gnome.desktop.interface font-name 'Noto Sans Regular 11'",
+        os.path.expanduser("~"),
+    )
 
-		file = open(multilib_conf, 'rb')
-		content = file.read().decode()
-		file.close()
-		file = open(multilib_conf, 'wb')
-		file.write(content.replace('#[multilib]\n#Include = /etc/pacman.d/mirrorlist','[multilib]\nInclude = /etc/pacman.d/mirrorlist').encode())
-		file.close()
-	except Exception:
-		pass
+    # Ly dm
+    if do_ly_dm:
+        cmdrun("sudo systemctl enable ly", os.path.expanduser("~"))
 
-
-
-	# Screenshare
-	cmdrun('sudo systemctl --user enable --now pipewire pipewire-pulse wireplumber', os.path.expanduser('~'))
-
-
-
-	# Network manager
-	cmdrun('sudo systemctl enable NetworkManager.service', os.path.expanduser('~'))
-
-
-
-	# Default dark mode
-	cmdrun("gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark", os.path.expanduser('~'))
-	cmdrun("gsettings set org.gnome.desktop.interface color-scheme prefer-dark", os.path.expanduser('~'))
-	cmdrun("gsettings set org.gnome.desktop.interface icon-theme Papirus", os.path.expanduser('~'))
-	cmdrun("gsettings set org.gnome.desktop.interface font-name 'Noto Sans Regular 11'", os.path.expanduser('~'))
-
-
-
-	# Ly dm
-	if do_ly_dm:
-		cmdrun('sudo systemctl enable ly', os.path.expanduser('~'))
-
-
-
-	# Reboot
-	if do_reboot:
-		cmdrun('sudo reboot', os.path.expanduser('~'))
+    # Reboot
+    if do_reboot:
+        cmdrun("sudo reboot", os.path.expanduser("~"))
